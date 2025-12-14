d22be439362ad7532900dc7194589214a7fc6726
fatmaputri <fatmaputriagustina99@gmail.com>
2025-12-14 20:42:55 +0700

    Pertemuan 4B: Getter dan Setter

---
+++ pertemuan-4/Account.php
@@ -0,0 +1,21 @@
+<?php
+
+class Account {
+    private float $balance = 0;
+
+    public function getBalance(): float {
+        return $this->balance;
+    }
+
+    public function setBalance(float $amount): void {
+        if ($amount >= 0) {
+            $this->balance = $amount;
+        } else {
+            throw new InvalidArgumentException("Saldo tidak boleh negatif");
+        }
+    }
+}
+
+$acc = new Account();
+$acc->setBalance(-10);
+echo $acc->getBalance();
