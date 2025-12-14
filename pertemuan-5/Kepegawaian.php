7a242ffa49f3c5d4761a28650eae187c5e74c1f8
fatmaputri <fatmaputriagustina99@gmail.com>
2025-12-14 22:34:18 +0700

    Pertemuan 5B: Multi-level inheritance

---
+++ pertemuan-5/Kepegawaian.php
@@ -0,0 +1,49 @@
+<?php
+
+class Pegawai {
+    protected string $nama;
+    protected float $gajiDasar;
+
+    public function __construct(string $nama, float $gajiDasar) {
+        $this->nama = $nama;
+        $this->gajiDasar = $gajiDasar;
+    }
+
+    public function hitungGaji(): float {
+        return $this->gajiDasar;
+    }
+}
+
+class PegawaiTetap extends Pegawai {
+    protected float $tunjanganTetap = 1500000;
+
+    public function hitungGaji(): float {
+        return parent::hitungGaji() + $this->tunjanganTetap;
+    }
+}
+
+class Manajer extends PegawaiTetap {
+    protected float $tunjanganJabatan = 2000000;
+
+    public function hitungGaji(): float {
+        return parent::hitungGaji() + $this->tunjanganJabatan;
+    }
+}
+
+class Direktur extends Manajer {
+    protected float $tunjanganDirektur = 3000000;
+
+    public function hitungGaji(): float {
+        return parent::hitungGaji() + $this->tunjanganDirektur;
+    }
+}
+
+
+$dina = new PegawaiTetap("Dina", 5000000);
+$faris = new Manajer("Faris", 7000000);
+$andi = new Direktur("Andi", 10000000);
+
+echo "Gaji Dina: Rp" . $dina->hitungGaji() . "\n";
+echo "Gaji Faris: Rp" . $faris->hitungGaji() . "\n";
+echo "Gaji Andi: Rp" . $andi->hitungGaji() . "\n";
+
