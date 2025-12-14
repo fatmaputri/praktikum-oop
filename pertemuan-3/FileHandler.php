6a41e12a66fbf3b7aa566c71ed485de7344c7c5a
fatmaputri <fatmaputriagustina99@gmail.com>
2025-12-14 15:39:36 +0700

    Praktikum G: file handling dengan constructor/destructor

---
+++ pertemuan-3/FileHandler.php
@@ -0,0 +1,19 @@
+<?php 
+class FileHandler { 
+private $handle; 
+public function __construct(string $filename) { 
+echo "Membuka file...\n"; 
+$this->handle = fopen($filename, "w"); 
+} 
+public function write(string $text) { 
+fwrite($this->handle, $text); 
+} 
+public function __destruct() { 
+echo "Menutup file...\n"; 
+fclose($this->handle); 
+} 
+} 
+$f = new FileHandler("/forbidden/output.txt");
+
+$f->write("Halo, ini baris pertama\n"); 
+$f->write("Ini baris kedua\n"); 
